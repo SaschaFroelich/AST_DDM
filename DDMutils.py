e3da253c6423f7edc3f6821deb09cac477fa7367
Sascha <sascha.froelich@gmail.com>
2024-08-05 10:07:37 +0200

    1.4: Modified DDMUtils

--- DDMutils.py
+++ DDMutils.py
@@ -5,7 +5,6 @@ Created on Sun Jul  7 11:21:30 2024
 
 @author: sascha
 """
-
 import pandas as pd
 import numpy as np
 
@@ -17,13 +16,12 @@ def get_DDM_data(day):
     # Check the first few rows of the data
     print(data.head())
 
-    #Jokertype 1 = random, 2 = congruent, 3 incongruent
-    data['jokercondition'] = data['jokertypes'] +1
+    # Jokertype 1 = random, 2 = congruent, 3 incongruent
+    data['jokercondition'] = data['jokertypes'] + 1
     data['jokercondition'] = pd.Categorical(data['jokercondition'], ordered=True, categories=[1, 2, 3])
 
-    #data["jokercondition"] = data["jokercondition"].map({1: "random", 2: "congruent",3: "incongruent"})
-    #recode response column
-    data['response'] = data['choices_GD'].replace(0,-1)
+    # Recode response column
+    data['response'] = data['choices_GD'].replace(0, -1)
 
     '''
         Create random q-value diff
@@ -37,34 +35,78 @@ def get_DDM_data(day):
     data_day1 = data[data['day'] == 1]
     data_day2 = data[data['day'] == 2]
 
-    '''
-        Save variables for ddm in ddmdata
-    '''
-    # ddmdata = pd.DataFrame(
-    #     np.column_stack([
-    #         data_day1["PB"],
-    #         data_day1["RT"],
-    #         data_day1["response"],
-    #         data_day1["qdiff"],
-    #         data_day1["repdiff"],
-    #         data_day1['jokercondition']
-    #     ]),  # Make sure this closing bracket matches with np.column_stack opening
-    #     columns=["subject", "rt", "response", "qdiff", "repdiff", "jokercondition"]
-    # )          
-
     if day == 2:
         ddmdata = pd.DataFrame({'subject': data_day2['PB'],
                                 'rt': data_day2['RT'],
                                 'response': data_day2['response'],
                                 'jokercondition': data_day2['jokercondition']})       
         
+    elif day == 1: 
+        ddmdata = pd.DataFrame({'subject': data_day1['PB'],
+                                'rt': data_day1['RT'],
+                                'response': data_day1['response'],
+                                'jokercondition': data_day1['jokercondition']})       
+        
 
-    ddmdata['jokercondition'] = ddmdata['jokercondition'].map(lambda x: 'R' if x == 1 else 
-                                                              'C' if x == 2 else 
-                                                              'I' if x== 3 else None)
+    # ddmdata['jokercondition'] = ddmdata['jokercondition'].map(lambda x: 'R' if x == 1 else 
+    #                                                           'C' if x == 2 else 
+    #                                                           'I' if x== 3 else None)
     
     assert None not in ddmdata['jokercondition']
 
-    ddmdata['subject'] = ddmdata['subject'].astype(int)
-    ddmdata['subject'] = ddmdata['subject'] - 1
+    # ddmdata['subject'] = ddmdata['subject'].astype("object")
+    # ddmdata['subject'] = ddmdata['subject'] - 1
+    return ddmdata
+
+def get_DDM_data2(day):
+    filename = 'ddm_data_gefiltert_choiceonly.csv'
+    data = pd.read_csv(filename)
+    
+    # Check the first few rows of the data
+    print(data.head())
+
+    #Jokertype 1 = random, 2 = congruent, 3 incongruent
+    data['jokercondition'] = data['jokertypes'] +1
+    data['jokercondition'] = pd.Categorical(data['jokercondition'], ordered=True, categories=[1,2,3])
+
+    #data["jokercondition"] = data["jokercondition"].map({1: "random", 2: "congruent",3: "incongruent"})
+    #recode response column
+    data['response'] = data['choices_GD'].replace(0,-1)
+    
+    #create q-value diff
+    random_variable = np.random.uniform(low=0.4, high=0.6, size= len(data))
+    data['qdiff'] = random_variable
+    
+    
+    #create distinct dataframes for day1 and 2
+    
+    data_day1 = data[data['day'] == 1]
+    data_day2 = data[data['day'] == 2]
+    
+    if day == 1:
+        ddmdata = pd.DataFrame(
+            np.column_stack([
+                data_day1["PB"],
+                data_day1["RT"],
+                data_day1["response"],
+                data_day1['jokercondition']
+            ]),  # Make sure this closing bracket matches with np.column_stack opening
+            columns=["subject", "rt", "response", "jokercondition"]
+        )     
+    
+    elif day == 2:
+        ddmdata = pd.DataFrame(
+            np.column_stack([
+                data_day2["PB"],
+                data_day2["RT"],
+                data_day2["response"],
+                data_day2['jokercondition']
+            ]),  # Make sure this closing bracket matches with np.column_stack opening
+            columns=["subject", "rt", "response", "jokercondition"]
+        )     
+    
+    print("Check that the data for the days are returned correctly!")
+    
+    ddmdata.rename(columns={'subject': 'participant_id'}, inplace = True)
+    
     return ddmdata
