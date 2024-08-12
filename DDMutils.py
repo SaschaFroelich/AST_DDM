61b4463571a980fbc23e14d76033af207044ed70
Sascha <sascha.froelich@gmail.com>
2024-08-12 08:19:54 +0200

    1.6: Added RL_model.py and changes to DDMUtil to include Qdiff, repdiff and min between the two.

--- DDMutils.py
+++ DDMutils.py
@@ -8,105 +8,220 @@ Created on Sun Jul  7 11:21:30 2024
 import pandas as pd
 import numpy as np
 
-def get_DDM_data(day):
-    # Assuming the working directory is set to where the file is located
-    filename = 'ddm_data_gefiltert_choiceonly.csv'
-    data = pd.read_csv(filename)
+def find_resp_options(stimulus_mat):
+    '''
+    Given a dual-target stimulus (e.g. 12, 1-indexed), this function returns the two response
+    options in 0-indexing. E.g.: stimulus_mat=14 -> option1_python = 0, option1_python = 3.
+    Returns twice the same option in case of STT.
 
+    Parameters
+    ----------
+    stimulus_mat : torch tensor with shape [num_agents]
+        DESCRIPTION.
+
+    Returns
+    -------
+    option1_python : tensor with shape [num_agents]
+        -1 in case of single-target trial.
+        Otherwise option 1 of dual-target trial. 0-indexed.
+    
+    option2_python : tensor with shape [num_agents]
+        -1 in case of single-target trial.
+        Otherwise option 1 of dual-target trial. 0-indexed.
+
+    '''
+    
+    assert isinstance(stimulus_mat, int)
+    
+    # # assert(torch.is_tensor(stimulus_mat))
+    option2_python = int(((stimulus_mat % 10) - 1))
+    option1_python = int((((stimulus_mat - (stimulus_mat % 10)) / 10) -1))
+    
+    "Make sure to return option2 twice in case of STT"
+    # option1_python = np.where(stimulus_mat > 10, option1_python, option2_python)
+    
+    return option1_python, option2_python
+
+def return_qdiff(row):
+    '''
+        qpdiff = Q(optimal_response) - Q(suboptimal_response)
+    '''
+
+    opt1, opt2 = find_resp_options(row['trialsequence'])
+    
+    if row['group'] <= 1:
+        optimal_responses = [0, 3]
+        
+    elif row['group'] > 1:
+        optimal_responses = [1, 2]
+    
+    assert int(opt1 in optimal_responses) + int(opt2 in optimal_responses) == 1
+    
+    # print(f"{row['trialsequence']} -> {opt1}, {opt2}.")
+
+    Q1 = row['Qs'][opt1]
+    Q2 = row['Qs'][opt2]
+    
+    if opt1 in optimal_responses:
+        res = Q1 - Q2
+    
+    elif opt2 in optimal_responses:
+        res = Q2 - Q1
+        
+    else:
+        raise Exception("Nope.")
+    
+    # if row['ID'] == '5d7ebf9e93902b0001965912' and row['trialidx'] == 19:
+    #     print(f"returning {res}.")
+        
+    return res
+
+def return_repdiff(row):
+    '''
+        repdiff = rep(optimal_response) - rep(suboptimal_response)
+    '''
+
+    opt1, opt2 = find_resp_options(row['trialsequence'])
+    
+    if row['group'] <= 1:
+        optimal_responses = [0, 3]
+        
+    elif row['group'] > 1:
+        optimal_responses = [1, 2]
+    
+    assert int(opt1 in optimal_responses) + int(opt2 in optimal_responses) == 1
+
+    # print(f"{row['trialsequence']} -> {opt1}, {opt2}.")
+
+    R1 = row['repvals'][opt1]
+    R2 = row['repvals'][opt2]
+    
+    if opt1 in optimal_responses:
+        res = R1 - R2
+    
+    elif opt2 in optimal_responses:
+        res = R2 - R1
+    
+    return res
+    
+# def get_DDM_data(day):
+#     # Assuming the working directory is set to where the file is located
+#     filename = 'ddm_data_gefiltert_choiceonly.csv'
+#     data = pd.read_csv(filename)
+
+#     # Check the first few rows of the data
+#     print(data.head())
+
+#     # Jokertype 1 = random, 2 = congruent, 3 incongruent
+#     data['jokercondition'] = data['jokertypes'] + 1
+#     data['jokercondition'] = pd.Categorical(data['jokercondition'], ordered=True, categories=[1, 2, 3])
+
+#     # Recode response column
+#     data['response'] = data['choices_GD'].replace(0, -1)
+
+#     '''
+#         Create random q-value diff
+#     '''
+#     random_variable = np.random.uniform(low=0.4, high=0.6, size= len(data))
+#     data['qdiff'] = random_variable
+
+#     '''
+#         Create distinct dataframes for day1 and 2
+#     '''
+#     data_day1 = data[data['day'] == 1]
+#     data_day2 = data[data['day'] == 2]
+
+#     if day == 2:
+#         ddmdata = pd.DataFrame({'subject': data_day2['PB'],
+#                                 'rt': data_day2['RT'],
+#                                 'response': data_day2['response'],
+#                                 'jokercondition': data_day2['jokercondition']})       
+        
+#     elif day == 1: 
+#         ddmdata = pd.DataFrame({'subject': data_day1['PB'],
+#                                 'rt': data_day1['RT'],
+#                                 'response': data_day1['response'],
+#                                 'jokercondition': data_day1['jokercondition']})       
+        
+
+#     # ddmdata['jokercondition'] = ddmdata['jokercondition'].map(lambda x: 'R' if x == 1 else 
+#     #                                                           'C' if x == 2 else 
+#     #                                                           'I' if x== 3 else None)
+    
+#     assert None not in ddmdata['jokercondition']
+
+#     # ddmdata['subject'] = ddmdata['subject'].astype("object")
+#     # ddmdata['subject'] = ddmdata['subject'] - 1
+#     return ddmdata
+
+def get_DDM_data2(day, RL = False):
+    import ast
+    # filename = 'ddm_data_gefiltert_choiceonly.csv'
+    # data = pd.read_csv(filename)
+
+    data = pd.read_csv('Data_DDM_2024-08-08 14:27:50.csv')
+    data = data[data['trialsequence']>10]
+    
+    data['Qs']  = data[['Q1', 'Q2', 'Q3', 'Q4']].apply(lambda row: list(row), axis=1)
+    
+    print("RT to sec")
+    data['RT'] /= 1_000
+    
+    print("Filtering out data < 150ms.")
+    data = data[data['RT'] > 0.15]
+    
+    print("Filtering out erroneous responses.")
+    data = data[data['choices'] != -2]
+    
     # Check the first few rows of the data
     print(data.head())
-
-    # Jokertype 1 = random, 2 = congruent, 3 incongruent
-    data['jokercondition'] = data['jokertypes'] + 1
-    data['jokercondition'] = pd.Categorical(data['jokercondition'], ordered=True, categories=[1, 2, 3])
-
-    # Recode response column
-    data['response'] = data['choices_GD'].replace(0, -1)
-
-    '''
-        Create random q-value diff
-    '''
-    random_variable = np.random.uniform(low=0.4, high=0.6, size= len(data))
-    data['qdiff'] = random_variable
-
-    '''
-        Create distinct dataframes for day1 and 2
-    '''
-    data_day1 = data[data['day'] == 1]
-    data_day2 = data[data['day'] == 2]
-
-    if day == 2:
-        ddmdata = pd.DataFrame({'subject': data_day2['PB'],
-                                'rt': data_day2['RT'],
-                                'response': data_day2['response'],
-                                'jokercondition': data_day2['jokercondition']})       
-        
-    elif day == 1: 
-        ddmdata = pd.DataFrame({'subject': data_day1['PB'],
-                                'rt': data_day1['RT'],
-                                'response': data_day1['response'],
-                                'jokercondition': data_day1['jokercondition']})       
-        
-
-    # ddmdata['jokercondition'] = ddmdata['jokercondition'].map(lambda x: 'R' if x == 1 else 
-    #                                                           'C' if x == 2 else 
-    #                                                           'I' if x== 3 else None)
     
-    assert None not in ddmdata['jokercondition']
-
-    # ddmdata['subject'] = ddmdata['subject'].astype("object")
-    # ddmdata['subject'] = ddmdata['subject'] - 1
-    return ddmdata
-
-def get_DDM_data2(day):
-    filename = 'ddm_data_gefiltert_choiceonly.csv'
-    data = pd.read_csv(filename)
-    
-    # Check the first few rows of the data
-    print(data.head())
+    if 'Unnamed: 0' in data.columns:
+        data.drop(columns=['Unnamed: 0'], axis=1, inplace = True)
 
     #Jokertype 1 = random, 2 = congruent, 3 incongruent
-    data['jokercondition'] = data['jokertypes'] +1
+    data['jokercondition'] = data['jokertypes'] + 1
     data['jokercondition'] = pd.Categorical(data['jokercondition'], ordered=True, categories=[1,2,3])
 
     #data["jokercondition"] = data["jokercondition"].map({1: "random", 2: "congruent",3: "incongruent"})
     #recode response column
     data['response'] = data['choices_GD'].replace(0,-1)
     
-    #create q-value diff
-    random_variable = np.random.uniform(low=0.4, high=0.6, size= len(data))
-    data['qdiff'] = random_variable
-    
+    data['repvals'] = data['repvals'].apply(ast.literal_eval)
+
+    # Create q-value diff
+    data['qdiff'] = data.apply(lambda row: return_qdiff(row), axis = 1)
+    data['repdiff'] = data.apply(lambda row: return_repdiff(row), axis = 1)
     
     #create distinct dataframes for day1 and 2
-    
     data_day1 = data[data['day'] == 1]
     data_day2 = data[data['day'] == 2]
     
     if day == 1:
         ddmdata = pd.DataFrame(
             np.column_stack([
-                data_day1["PB"],
+                data_day1["ag_idx"],
                 data_day1["RT"],
                 data_day1["response"],
-                data_day1['jokercondition']
+                data_day1['jokercondition'],
+                data_day1['qdiff'],
+                data_day1['repdiff']
             ]),  # Make sure this closing bracket matches with np.column_stack opening
-            columns=["subject", "rt", "response", "jokercondition"]
+            columns=["participant_id", "rt", "response", "jokercondition", "qdiff", "repdiff"]
         )     
     
     elif day == 2:
         ddmdata = pd.DataFrame(
             np.column_stack([
-                data_day2["PB"],
+                data_day2["ag_idx"],
                 data_day2["RT"],
                 data_day2["response"],
-                data_day2['jokercondition']
+                data_day2['jokercondition'],
+                data_day2["qdiff"],
+                data_day2['repdiff']
             ]),  # Make sure this closing bracket matches with np.column_stack opening
-            columns=["subject", "rt", "response", "jokercondition"]
+            columns=["participant_id", "rt", "response", "jokercondition", "qdiff", "repdiff"]
         )     
     
     print("Check that the data for the days are returned correctly!")
-    
-    ddmdata.rename(columns={'subject': 'participant_id'}, inplace = True)
-    
     return ddmdata
---
+++ simple_model_hier_console.py
@@ -0,0 +1,301 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 10:07:50 2024
+
+@author: sascha
+"""
+
+import numpyro
+import numpyro.distributions as dist
+from numpyro.infer import MCMC, NUTS
+numpyro.set_host_device_count(4)
+from jax import random
+import jax
+
+# Ensure JAX uses all available CPU cores
+jax.config.update('jax_platform_name', 'cpu')
+# jax.config.update('jax_platform_name', 'gpu')
+jax.config.update('jax_enable_x64', True)
+
+import time
+from datetime import datetime
+import pickle
+import numpy as np
+import matplotlib as plt
+import hssm
+import tqdm as notebook_tqdm
+import os
+import pandas as pd
+import DDMutils
+import bambi as bmb
+import arviz as az
+
+hssm.set_floatX("float64")
+
+'''
+    Individual participants, all jokertypes
+    With v ~ 1 + jokercondition
+    i.e., not hierarchically modelled
+'''
+
+# ddmdata = ddmdata[ddmdata['jokercondition'] == 2]
+# time.sleep(36_000)
+
+R_thresh = 1.05
+
+for day in range(1, 2):
+    ddmdata = DDMutils.get_DDM_data2(day)
+    ddmdata = ddmdata[ddmdata['participant_id']<=10]
+    
+    ddmdata.rename(columns={'subject': 'participant_id'}, inplace = True)
+    ddmdata['jokercondition'] = ddmdata['jokercondition'].apply(lambda x: 'A' if x == 1 else 'B' if x == 2 else 'C' if x == 3 else None)
+    
+    n_draws = 6_0
+    n_tune = 6_0
+    repeat = 1
+    num_rep = 1
+    
+    initvalues = {'a_Intercept': 0.8, 
+                  'a_1|participant_id_mu': 0,
+                  't_Intercept': 0.2, 
+                  't_1|participant_id_mu': 0,
+                  'theta_Intercept': 0.9, 
+                  'theta_1|participant_id_mu': 0,
+                  'z_Intercept': 0.5,
+                  'z_jokercondition': [0., 0.],
+                  'z_jokercondition|participant_id_mu': [0., 0.],
+                  'v_Intercept': 2.0,
+                  'v_jokercondition': [0., 0.]} # a, p, t, v, z
+    
+    while repeat and num_rep < 4:
+        model_reg_v_angle_hier = hssm.HSSM(
+            data = ddmdata,
+            model = "angle",
+            hierarchical = True,
+            categorical = 'jokercondition',
+            include=[
+                {
+                    "name": "v",
+                    "formula": "v ~ 1 + jokercondition + (0 + jokercondition|participant_id)",
+                    "prior": {
+                        # "jokercondition": {"name": "Normal", 
+                        #                       "mu": {"name": "Normal", "mu": 2., "sigma": 1.},
+                        #                       "sigma": {"name": "HalfNormal", "sigma": .3}},
+                        "jokercondition|participant_id": {"name": "Normal", 
+                                              "mu": {"name": "Normal", "mu": 2., "sigma": 1.},
+                                              "sigma": {"name": "HalfNormal", "sigma": .3}},
+                        "Intercept": {"name": "Normal", "mu": 1, "sigma": 2, "initval": 1},
+                    },
+                    "link": "identity",
+                },
+                {
+                    "name": "z",
+                    "formula": "z ~ 1 + jokercondition + (0 + jokercondition|participant_id)",
+                    "prior": {
+                        "Intercept": {
+                            "name": "Uniform",
+                            "lower": 0.3,
+                            "upper": 0.7,
+                            "initval": 0.5},
+                    },
+                    "link": "identity",
+                },
+                {
+                    "name": "a",
+                    "formula": "a ~  1 + (1|participant_id)",
+                    "prior": {
+                        "Intercept": {
+                            "name": "Normal",
+                            "mu": 0.8,
+                            "sigma": 0.1,
+                            "initval": 0.8},
+                        "1|participant_id": {"name": "Normal", 
+                                              "mu": {"name": "Normal", "mu": 0., "sigma": 1.},
+                                              "sigma": {"name": "HalfNormal", "sigma": .3}},
+                    },
+                    "link": "identity",
+                },
+                {
+                    "name": "t",
+                    "formula": "t ~  1 + (1|participant_id)",
+                    "prior": {
+                        "Intercept": {
+                            "name": "Normal",
+                            "mu": 0.25,
+                            "sigma": 0.05,
+                            "initval": 0.25},
+                        "1|participant_id": {"name": "Normal", 
+                                              "mu": {"name": "Normal", "mu": 0, "sigma": 0.5},
+                                              "sigma": {"name": "HalfNormal", "sigma": .3}},
+                    },
+                    "link": "identity",
+                },
+                {
+                    "name": "theta",
+                    "formula": "theta ~  1 + (1|participant_id)",
+                    "prior": {
+                        "Intercept": {
+                            "name": "Normal",
+                            "mu": 1,
+                            "sigma": 0.3,
+                            "initval": 0.9},
+                        "1|participant_id": {"name": "Normal", 
+                                              "mu": {"name": "Normal", "mu": 0, "sigma": 0.5},
+                                              "sigma": {"name": "HalfNormal", "sigma": .3}},
+                    },
+                    "link": "identity",
+                },
+                
+                # {
+                #     "name": "t",
+                #     "formula": "t ~  1 + (1|participant_id)",
+                #     "prior": {
+                #         # "Intercept": {
+                #         #     "name": "Uniform",
+                #         #     "lower": 0.3,
+                #         #     "upper": 0.7,
+                #         #     "initval": 0.5},
+                #         # "1|participant_id": {
+                #         #     "name": "Uniform",
+                #         #     "lower": 0.3,
+                #         #     "upper": 0.7,
+                #         #     "initval": 0.5},
+                #     },
+                #     "link": "identity",
+                # },
+                # {
+                #     "name": "theta",
+                #     "formula": "theta ~  1 + (1|participant_id)",
+                #     "prior": {
+                #         # "Intercept": {
+                #         #     "name": "Uniform",
+                #         #     "lower": 0.3,
+                #         #     "upper": 0.7,
+                #         #     "initval": 0.5},
+                #         # "1|participant_id": {
+                #         #     "name": "Uniform",
+                #         #     "lower": 0.3,
+                #         #     "upper": 0.7,
+                #         #     "initval": 0.5},
+                #     },
+                #     "link": "identity",
+                # },
+            ],
+            )
+            
+        
+        infer_data_trace = model_reg_v_angle_hier.sample(
+            sampler="nuts_numpyro", chains=4, cores=4, draws=n_draws, tune=n_tune,
+            include_mean = False
+            )
+        
+        if np.any(az.summary(infer_data_trace, var_names = ['a_Intercept',
+                                                  'a_1|participant_id_mu',
+                                                  't_Intercept',
+                                                  't_1|participant_id_mu',
+                                                  'theta_Intercept',
+                                                  'theta_1|participant_id_mu',
+                                                  'v_Intercept', 
+                                                  'v_jokercondition',
+                                                  'v_jokercondition|participant_id_mu',
+                                                  'z_Intercept',
+                                                  'z_jokercondition',
+                                                  'z_jokercondition|participant_id_mu'])['r_hat']>R_thresh):
+            n_draws += 1000
+            n_tune += 1000
+            num_rep += 1
+            
+            '''
+                Set initial values of those parameters with rhat < 1.05
+            '''
+            if np.any(az.summary(infer_data_trace, var_names = ['a_Intercept',
+                                                      'a_1|participant_id_mu',
+                                                      't_Intercept',
+                                                      't_1|participant_id_mu',
+                                                      'theta_Intercept',
+                                                      'theta_1|participant_id_mu',
+                                                      'v_Intercept', 
+                                                      'v_jokercondition',
+                                                      'v_jokercondition|participant_id_mu',
+                                                      'z_Intercept',
+                                                      'z_jokercondition',
+                                                      'z_jokercondition|participant_id_mu'])['r_hat']<=R_thresh):
+                
+                
+                
+                pass
+    
+            
+            dfgh
+            
+        else:
+            repeat = 0
+
+            print(f"Saving Results for pid {pid}.")
+            summary = az.summary(infer_data_trace, var_names = ['a_Intercept',
+                                                      'a_1|participant_id_mu',
+                                                      'a_1|participant_id_sigma',
+                                                      't_Intercept',
+                                                      't_1|participant_id_mu',
+                                                      't_1|participant_id_sigma',
+                                                      'theta_Intercept',
+                                                      'theta_1|participant_id_mu',
+                                                      'theta_1|participant_id_sigma',
+                                                      'v_Intercept', 
+                                                      'v_jokercondition',
+                                                      'v_jokercondition|participant_id_mu',
+                                                      'v_jokercondition|participant_id_sigma',
+                                                      'z_Intercept',
+                                                      'z_jokercondition',
+                                                      'z_jokercondition|participant_id_mu',
+                                                      'z_jokercondition|participant_id_sigma'])
+            
+            
+            timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+            pickle.dump( summary, 
+                        open(f"DDMAngleNolapsesAlljokerswIntercept/Hierarchical_model_{timestamp}_Day{day}.p", "wb" ) )
+        
+        print("== == == == ==")
+
+summary = az.summary(infer_data_trace)        
+
+#%%
+for rowidx in range(len(summary)):
+    if 'a' in summary.index[rowidx] and summary.index[rowidx][0:2] != 'a[' and summary.index[rowidx][0:2] != 'z_' and summary.index[rowidx][0:2] != 'v_':
+        print(summary.index[rowidx])
+
+print("\n\nPrinting mu\n")    
+for rowidx in range(len(summary)):
+    if 'mu' in summary.index[rowidx]:
+        print(summary.index[rowidx])
+        
+print("\n\nPrinting sigma\n")    
+for rowidx in range(len(summary)):
+    if 'sigma' in summary.index[rowidx]:
+        print(summary.index[rowidx])
+        
+print("\n\nPrinting v_\n")    
+for rowidx in range(len(summary)):
+    if 'v_' in summary.index[rowidx]:
+        print(summary.index[rowidx])
+        
+print("\n\nPrinting z_\n")    
+for rowidx in range(len(summary)):
+    if 'v_' in summary.index[rowidx]:
+        print(summary.index[rowidx])
+    
+#%%
+
+az.summary(infer_data_trace, var_names = ['a_Intercept',
+                                          'a_1|participant_id_mu',
+                                          't_Intercept',
+                                          't_1|participant_id_mu',
+                                          'theta_Intercept',
+                                          'theta_1|participant_id_mu',
+                                          'v_Intercept', 
+                                          'v_jokercondition',
+                                          'v_jokercondition|participant_id_mu',
+                                          'z_Intercept',
+                                          'z_jokercondition',
+                                          'z_jokercondition|participant_id_mu'])['r_hat']
