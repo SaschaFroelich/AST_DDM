468a65e271c5a9f1d73719b2c059034f4ec5f936
Sascha <sascha.froelich@gmail.com>
2024-08-14 11:27:01 +0200

    1.9: simple_model_console.py now can also fit lapses, and sets initial values for parameters with Rhat <= R_thresh.

--- simple_model_console.py
+++ simple_model_console.py
@@ -41,82 +41,181 @@ hssm.set_floatX("float64")
 '''
 
 # ddmdata = ddmdata[ddmdata['jokercondition'] == 2]
-
 # time.sleep(36_000)
 
 R_thresh = 1.05
+with_lapses = False
 
-for day in range(2, 3):
+for day in range(1, 2):
     
     ddmdata = DDMutils.get_DDM_data2(day)
     
     initvalues = {'a': 0.8, 
                   't': 0.05,
                   'theta': 0.2, 
-                  'z': 0.5,
-                  'v': 2.0} # a, p, t, v, z
+                  'z_Intercept': 0.5,
+                  'v_Intercept': 2.0,
+                  'v_jokercondition': [0, 0],
+                  'z_jokercondition': [0, 0]}
     
     ddmdata.rename(columns={'subject': 'participant_id'}, inplace = True)
     
-    for pid in range(34, 60):
-        n_draws = 1000
-        n_tune = 1000
+    for pid in [18, 20, 26, 31, 34, 36, 38, 41, 43, 45, 48, 49, 51, 58, 60]:
+        n_draws = 2_0
+        n_tune = 2_0
         repeat = 1
         num_rep = 1
         
         while repeat and num_rep < 4:        
             print(f"Inference for pid {pid} on day {day}. Repetition number {num_rep}.")    
             ddmdata_onesub = ddmdata[ddmdata['participant_id'] == pid]
-            model_reg_v_angle_hier = hssm.HSSM(
-                data = ddmdata_onesub,
-                model = "angle",
-                hierarchical = False,
-                categorical = 'jokercondition',
-                include=[
-                    {
-                        "name": "v",
-                        "formula": "v ~ 1 + jokercondition",
-                        "prior": {
-                            "Intercept": {"name": "Normal", "mu": 1, "sigma": 2, "initval": 1},
+
+            if with_lapses:
+                model_reg_v_angle = hssm.HSSM(
+                    data = ddmdata_onesub,
+                    model = "angle",
+                    hierarchical = False,
+                    categorical = 'jokercondition',
+                    p_outlier = {"name": "Uniform", "lower": 0.0001, "upper": 0.5},
+                    lapse = bmb.Prior("Uniform", lower=0.0, upper=20.0),
+                    include = [
+                        {
+                            "name": "v",
+                            "formula": "v ~ 1 + jokercondition",
+                            "prior": {
+                                "Intercept": {"name": "Normal", 
+                                              "mu": 1, 
+                                              "sigma": 2, 
+                                              "initval": initvalues['v_Intercept']},
+                                
+                                "jokercondition": {"name": 
+                                                   "Normal", 
+                                                   "mu": 0, 
+                                                   "sigma": 2, 
+                                                   "initval": initvalues['v_jokercondition']},
+                            },
+                            "link": "identity",
                         },
-                        "link": "identity",
-                    },
-                    {
-                        "name": "z",
-                        "formula": "z ~ 1 + jokercondition",
-                        "prior": {
-                            "Intercept": {
-                                "name": "Uniform",
-                                "lower": 0.3,
-                                "upper": 0.7,
-                                "initval": 0.5},
+                        {
+                            "name": "z",
+                            "formula": "z ~ 1 + jokercondition",
+                            "prior": {
+                                "Intercept": {
+                                    "name": "Uniform",
+                                    "lower": 0.3,
+                                    "upper": 0.7,
+                                    "initval": initvalues['z_Intercept']},
+                                
+                                "jokercondition": {"name": "Normal", 
+                                                   "mu": 0, 
+                                                   "sigma": 2, 
+                                                   "initval": initvalues['z_jokercondition']},
+                            },
+                            "link": "identity",
+                        }
+                    ],
+                )
+                
+            else:
+                model_reg_v_angle = hssm.HSSM(
+                    data = ddmdata_onesub,
+                    model = "angle",
+                    hierarchical = False,
+                    categorical = 'jokercondition',
+                    include=[
+                        # {
+                        #     "name": "a",
+                        #     "formula": "a ~ 1 + (1|participant_id)",
+                        #     "prior": {
+                        #         "Intercept": {"name": "Normal", 
+                        #                       "mu": 1, 
+                        #                       "sigma": 2, 
+                        #                       "initval": initvalues['a']},
+                        #         # "jokercondition": {"name": "Normal", 
+                        #         #                    "mu": 0, 
+                        #         #                    "sigma": 2, 
+                        #         #                    "initval": initvalues['v_jokercondition']},
+                        #     },
+                        #     "link": "identity",
+                        # },
+                        {
+                            "name": "v",
+                            "formula": "v ~ 1 + jokercondition",
+                            "prior": {
+                                "Intercept": {"name": "Normal", 
+                                              "mu": 1, 
+                                              "sigma": 2, 
+                                              "initval": initvalues['v_Intercept']},
+                                "jokercondition": {"name": "Normal", 
+                                                   "mu": 0, 
+                                                   "sigma": 2, 
+                                                   "initval": initvalues['v_jokercondition']},
+                            },
+                            "link": "identity",
                         },
-                        "link": "identity",
-                    }
-                ],
-            )
-            
-            
-            infer_data_reg_v_a = model_reg_v_angle_hier.sample(
+                        {
+                            "name": "z",
+                            "formula": "z ~ 1 + jokercondition",
+                            "prior": {
+                                "Intercept": {
+                                    "name": "Uniform",
+                                    "lower": 0.3,
+                                    "upper": 0.7,
+                                    "initval": initvalues['z_Intercept']},
+                                "jokercondition": {"name": "Normal", 
+                                                   "mu": 0, 
+                                                   "sigma": 2, 
+                                                   "initval": initvalues['z_jokercondition']},
+                            },
+                            "link": "identity",
+                        }
+                    ],
+                )
+                
+            infer_data_trace = model_reg_v_angle.sample(
                 sampler="nuts_numpyro", chains=4, cores=4, draws=n_draws, tune=n_tune,
                 include_mean = False
                 )
             
-            print(ddmdata_onesub['participant_id'].unique())
-            print(az.summary(infer_data_reg_v_a)[0:10])
+            summary = model_reg_v_angle.summary()
+            print(summary)
             
-            if ~np.all(az.summary(infer_data_reg_v_a)[0:10]['r_hat']<R_thresh):
-                n_draws += 1000
-                n_tune += 1000
+            if np.any(summary['r_hat'] > R_thresh):
+                n_draws += 2000
+                n_tune += 2000
                 num_rep += 1
+                
+                if np.any(summary['r_hat'] <= R_thresh):
+                    summary = summary[summary['r_hat'] <= R_thresh]
+                    
+                    for param in summary.index:
+                        if 'jokercondition' not in param:
+                            print(f"Setting {param}.")
+                            initvalues[param] = summary.loc[param, 'mean']
+
+                        elif 'v_jokercondition[' in param:
+                            index = int(param[-4])-1
+                            print(f"Setting {param} (index {index}).")
+                            initvalues['v_jokercondition'][index] = summary.loc[param, 'mean']
+
+                        elif 'z_jokercondition[' in param:
+                            index = int(param[-4])-1
+                            print(f"Setting {param} (index {index}).")
+                            initvalues['z_jokercondition'][index] = summary.loc[param, 'mean']
                 
             else:
                 repeat = 0
             
                 print("Saving Results.")
                 timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-                pickle.dump( az.summary(infer_data_reg_v_a)[0:14], 
-                            open(f"DDMAngleNolapsesAlljokerswIntercept/{timestamp}_Day{day}_pid{pid}.p", "wb" ) )
+                
+                if with_lapses:
+                    pickle.dump( model_reg_v_angle.summary(), 
+                                open(f"DDMAngleNolapsesAlljokerswInterceptwLapses/pid{pid}_{timestamp}_Day{day}.p", "wb" ) )
+                    
+                else:
+                    pickle.dump( model_reg_v_angle.summary(), 
+                                open(f"DDMAngleNolapsesAlljokerswIntercept/pid{pid}_{timestamp}_Day{day}.p", "wb" ) )                    
             
             print("== == == == ==")
             
